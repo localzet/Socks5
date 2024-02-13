277eaa6cda05efd949bdbbcc0268dc740c73db0e
localzet <creator@localzet.com>
2024-02-13 17:01:02 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = [
+    "auth" => [
+        METHOD_NO_AUTH => true,
+        METHOD_USER_PASS => function ($request) {
+            return $request['user'] == 'user' && $request['pass'] == 'pass';
+        }
+    ],
+    "log_level" => LOG_DEBUG,
+    "tcp_port" => 1080, // 1080 - порт SOCKS
+    "udp_port" => 0,
+    "wanIP" => '192.168.1.1',
+];
